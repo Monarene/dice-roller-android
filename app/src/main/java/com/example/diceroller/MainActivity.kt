6917d54184788d41c2132d6a8b4e9ddc7e2f263e
Michael Mekuleyi <mekuleyimichael@gmail.com>
2021-04-24 03:41:05 +0100

    implemented the onClick

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,9 +15,18 @@ class MainActivity : AppCompatActivity() {
         val rollButton : Button = findViewById(R.id.roll_button)
         rollButton.text = "Let's roll"
         rollButton.setOnClickListener {
-            Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+
+
     }
 
+    private fun rollDice() {
+        Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
+        val randomInt = Random().nextInt(6) + 1
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randomInt.toString()
+
+    }
 
 }
